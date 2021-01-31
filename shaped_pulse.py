764a521a27b35ca301c497b95b7e6e7f784c43f5
BebertDeChast <74964221+BebertDeChast@users.noreply.github.com>
2021-01-31 17:23:46 +0100

    creation of branch and test

---
+++ shaped_pulse.py
@@ -0,0 +1,10 @@
+# Created by Humbert de Chastellux the 31/01/2021
+
+
+def hello_world(number):
+    for i in range(number):
+        print("Hello World !")
+    return print("Good bye !")
+
+
+hello_world(10)
